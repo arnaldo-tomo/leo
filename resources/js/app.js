34a6546f6b462d6a9ef575882ae08a8f5883b2d6
Arnaldo tomo <arnaldotomo@gmail.com>
2025-05-15 22:16:54 +0200

    Update app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,14 @@
 import './bootstrap';
+import './face-recognition.js';
 
 import Alpine from 'alpinejs';
 
 window.Alpine = Alpine;
 
 Alpine.start();
+
+// Código de inicialização, se necessário
+document.addEventListener('DOMContentLoaded', function() {
+    // Inicialização global do aplicativo
+    console.log('Aplicativo inicializado');
+});
